f012499a63176b52e145db8382d1a101bd2502f5
asdnasdn <artimerum@gmail.com>
2020-04-03 20:54:50 +0300

    firebase-authors

---
+++ functions/index.js
@@ -0,0 +1,166 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express = require("express");
+const app = express();
+admin.initializeApp(functions.config().firebase);
+
+const db = admin.firestore();
+app.use(express.json());
+
+async function Check(CollectionName, name) {
+
+    const checkValue = await db.collection(CollectionName).where("name", "==", name).get();
+    console.log('checkValue.size: ', checkValue.size);
+    return checkValue.size;
+
+}
+
+async function GetAuthors(Book) {
+    const ref = await db.collection("Authors").get();
+    console.log('ref: ', ref);
+    const Authors = [];
+    ref.forEach(doc => {
+        let Docs = {
+            id: doc.id,
+            name: doc.data().name,
+            dateOfBirth: doc.data().dateOfBirth,
+            books: []
+        }
+        Authors.push(Docs);
+        if (!Book) {
+            return;
+        }
+        Book.forEach(el => {
+            if (el.author == doc.id) {
+                console.log(el)
+                Docs.books.push(el)
+            }
+        })
+    })
+    return Authors;
+}
+
+async function GetBooks() {
+    const ref = await db.collection("Books").get();
+    const Books = [];
+    ref.forEach(doc => {
+        Books.push({
+            id: doc.id,
+            name: doc.data().name,
+            author: doc.data().author
+        })
+    })
+    return Books
+}
+
+app.post('/createAuthor', async (req, res) => {
+
+    try {
+        let ExistAuthor = await Check('Authors', req.body.name)
+        console.log('ExistAuthor: ', ExistAuthor);
+        if (ExistAuthor > 0) {
+            res.status(400).send("{error : Author alredy exist}")
+            throw "Exsist";
+
+        }
+
+        let Author = {
+            name: req.body.name,
+            dateOfBirth: req.body.birth,
+        }
+
+        const NewDoc = await db.collection('Authors').add(Author);
+        res.status(200).send({
+            id: NewDoc.id
+        });
+    } catch (e) {
+        res.status(400).send(`{error : ${e}}`)
+    }
+
+})
+
+app.get('/getAuthors', async (req, res) => {
+
+    try {
+        let Authors = await GetAuthors()
+        res.status(200).send(Authors);
+    } catch (e) {
+        res.status(400).send(`{error : ${e}}`)
+    }
+
+})
+
+app.get('/authorAndBooks', async (req, res) => {
+    try {
+        let AllBooks = await GetBooks();
+        console.log('Books: ', AllBooks);
+        let Authors = await GetAuthors(AllBooks)
+
+        res.status(200).send(Authors)
+    } catch (e) {
+        res.status(400).send(`{e : ${e}}`)
+    }
+})
+
+app.get("/getAuthor/:id", async (req, res) => {
+
+    try {
+        let response = [];
+        const id = req.params.id
+        let ref = await db.collection('Authors').doc(id).get();
+        let Docs = {
+            id: id,
+            name: ref.data().name,
+            dateOfBirth: ref.data().dateOfBirth,
+            books: []
+        }
+        response.push(Docs)
+        let books = await db.collection('Books').where("author", '==', id.toString()).get();
+        await books.forEach(el => {
+            console.log(el.data())
+            Docs.books.push({
+                id: el.id,
+                name: el.data().name,
+                author: el.data().author
+            })
+        })
+        res.status(200).send(response)
+    } catch (e) {
+        res.status(400).send(`{error :${e}}`)
+    }
+
+})
+
+app.post('/createBook/:id', async (req, res) => {
+
+    try {
+        let ExistBook = await Check("Books", req.body.name)
+        if (ExistBook > 0) {
+            res.status(400).send(`{error :  Book alredy exist }`)
+            throw new Error();
+        }
+        let Book = {
+            name: req.body.name,
+            author: req.params.id
+        }
+        const newList = await db.collection('Books').add(Book);
+        res.status(200).send({
+            id: newList.id
+        });
+    } catch (e) {
+        res.status(400).send(`{error : ${e}}`)
+    }
+
+})
+
+app.get('/getBooks', async (req, res) => {
+
+    try {
+        let AllBooks = await GetBooks()
+        res.status(200).send(AllBooks);
+    } catch (e) {
+        res.status(400).send(`{error : ${e}}`)
+    }
+
+})
+exports.api = functions.https.onRequest(app);
